749764d36d6151458e01a7c915d7219d83838c23
bhuvanraj18 <147475656+bhuvanraj18@users.noreply.github.com>
2023-12-04 13:11:02 +0530

    #include<stdio.h> int main(){     int a,b,i,j,count;     scanf("%d %d",&a,&b);     for(i=a;i<=b;i++){         count=0;         for(j=1;j<i;j++){             if(i%j==0){                 count++;             }         }
        if(count==1){
            printf("%d
    ",j);
        }
        }
    }

---
+++ Generate_Prime_Numbers_In_A_Interval_.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a,b,i,j,count;
+    scanf("%d %d",&a,&b);
+    for(i=a;i<=b;i++){
+        count=0;
+        for(j=1;j<i;j++){
+            if(i%j==0){
+                count++;
+            }
+        }
+    
+    if(count==1){
+        printf("%d
+",j);
+    }
+    }
+}
