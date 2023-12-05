67753468a28f6e7ec8d9295aa2f02d124400782f
bhuvanraj18 <147475656+bhuvanraj18@users.noreply.github.com>
2023-12-05 11:26:03 +0530

    #include<stdio.h> #include<math.h> int main() {     int t,i;     scanf("%d",&t);     for(i=1;i<=t;i++)     {         int n,a;         scanf("%d",&n);         a=sqrt(n);         if((a*a)==n){             printf("True ");         }         else{             printf("False ");         }     } }

---
+++ Valid_Perfect_Square.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int t,i;
+    scanf("%d",&t);
+    for(i=1;i<=t;i++)
+    {
+        int n,a;
+        scanf("%d",&n);
+        a=sqrt(n);
+        if((a*a)==n){
+            printf("True
+");
+        }
+        else{
+            printf("False
+");
+        }
+    }  
+}
