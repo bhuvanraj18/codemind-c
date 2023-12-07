d36bf925093eee62b6c36a9905ae56ec3201dd47
bhuvanraj18 <147475656+bhuvanraj18@users.noreply.github.com>
2023-12-07 20:26:00 +0530

    #include<stdio.h> int main(){     int n,i,j;     scanf("%d",&n);     for(i=65;i<=64+n;i++){         for(j=65;j<=i;j++){             printf("%c ",j);         }         printf(" ");     } }

---
+++ Basic_Pattern_2.3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=65;i<=64+n;i++){
+        for(j=65;j<=i;j++){
+            printf("%c ",j);
+        }
+        printf("
+");
+    }
+}
