1e5a34b8b9b5179f8029ff8cf7dbc843d508d0cc
bhuvanraj18 <147475656+bhuvanraj18@users.noreply.github.com>
2023-12-23 20:39:53 +0530

    #include<stdio.h> int main(){     int a,b,n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++){         scanf("%d",&arr[i]);     }     scanf("%d %d",&a,&b);     int flag =0;     for(int i=0;i<n;i++){         int occ=0;         int vis=0;         for(int j=0;j<n;j++){         if(j<i && arr[i]==arr[j]){             vis=1;             break;         }         if(arr[i]==arr[j]){             occ++;         }         }         if(vis==0 && arr[i]==occ){             printf("%d ",arr[i]);             flag=1;         }     }     if(flag==0){         printf("-1");     } }

---
+++ Elements_same_as_count.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+    int a,b,n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d %d",&a,&b);
+    int flag =0;
+    for(int i=0;i<n;i++){
+        int occ=0;
+        int vis=0;
+        for(int j=0;j<n;j++){
+        if(j<i && arr[i]==arr[j]){
+            vis=1;
+            break;
+        }
+        if(arr[i]==arr[j]){
+            occ++;
+        }
+        }
+        if(vis==0 && arr[i]==occ){
+            printf("%d ",arr[i]);
+            flag=1;
+        }
+    }
+    if(flag==0){
+        printf("-1");
+    }
+}
